d3312e2d0b41f7dc290f22ca133eda31435c35cf
chris@mann.fr <chris@mann.fr>
2020-11-16 00:40:36 +0000

    Now I have some working content.

---
+++ src/pages/category.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {Link} from "gatsby"
+
+export default function CategoryRoute() {
+  return (
+    <>
+<Link to={"/"}>Mann.fr</Link>
+<h1>Categroy</h1>
+<p>Welcome to the category page.</p>
+
+    </>
+  )
+}
