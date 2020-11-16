9e211106abd39c06ed05e9286320ca2e3b6f8882
chris2fr <chris@mann.fr>
2020-11-16 14:11:40 +0100

    Looking good for initial content creation

---
+++ src/templates/default-mdx-layout.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+//import Img from "gatsby-image"
+
+
+export default ({ data, pageContext }) => {
+  let title = data.mdx && data.mdx.frontmatter && data.mdx.frontmatter.title;
+  let imgSrc = (data.mdx && data.mdx.frontmatter && data.mdx.frontmatter.feature_image)? data.mdx.frontmatter.feature_image.publicURL:null;
+  return (
+    <>
+      <h1>{title}</h1>
+      {imgSrc &&
+      <img src={imgSrc}/>
+      }
+      <div>
+        {data.mdx ? (
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        ) : (
+          <div>This page hasn't been translated yet</div>
+        )}
+      </div>
+      <h1>Context</h1>
+      <pre>{JSON.stringify(pageContext, null, 2)}</pre>
+    </>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(
+      fields: { slug: { eq: $slug } }  
+    ) {
+      frontmatter {
+        slug
+        title
+        feature_image {
+          publicURL
+        }
+      }
+      body
+    }
+  }
+`
