762acfd01d6ca34d313e1f0aea2202a76aa3f9c9
chris2fr <chris@mann.fr>
2022-03-25 09:23:08 +0100

    Typos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -125,7 +125,7 @@ export default function IndexRoute({ pageContext, disabled, data }) {
         <div style={{ "display": "flex", "justify-content": "space-around", "flex-wrap": "wrap" }}>
           <div style={{ "flex-grow": "1"}}>
             <p>
-            {t("statutdes")}
+            {t("statutdesc")}
               
               <br />
               +33 7 68 40 38 38
